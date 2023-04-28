ceffe486f562307a80daea6721280a93c72ea711
Vladimir <kosakvova@gmail.com>
2023-04-28 16:37:26 +0400

    mvc

---
+++ src/main/java/ru/mcclinics/orderservice/mcclinics/McclinicsCorsConfiguration.java
@@ -0,0 +1,43 @@
+package ru.mcclinics.orderservice.mcclinics;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class McclinicsCorsConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowCredentials(true)
+        .allowedOriginPatterns("*")
+        .allowedHeaders("*")
+        .allowedMethods("*")
+        .exposedHeaders(HttpHeaders.SET_COOKIE);
+  }
+
+  @Bean
+  public CorsFilter corsWebFilter(UrlBasedCorsConfigurationSource corsConfigurationSource) {
+    return new CorsFilter(corsConfigurationSource);
+  }
+
+  @Bean
+  public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration mcclinicsCorsConfiguration = new CorsConfiguration();
+    mcclinicsCorsConfiguration.setAllowCredentials(true);
+    mcclinicsCorsConfiguration.addAllowedHeader("*");
+    mcclinicsCorsConfiguration.addAllowedMethod("*");
+    mcclinicsCorsConfiguration.addAllowedOriginPattern("*");
+    mcclinicsCorsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+    UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+    corsConfigurationSource.registerCorsConfiguration("/**", mcclinicsCorsConfiguration);
+    return corsConfigurationSource;
+  }
+}
