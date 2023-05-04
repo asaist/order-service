1c6bee1f35f32cf3832267adf8665b150644cd07
kvn <v.n.kosinevskiy@samsmu.ru>
2023-05-04 18:11:13 +0400

    141

--- src/main/java/ru/mcclinics/orderservice/rest/AuthorController.java
+++ src/main/java/ru/mcclinics/orderservice/rest/AuthorController.java
@@ -1,17 +1,13 @@
 package ru.mcclinics.orderservice.rest;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.mcclinics.orderservice.domain.Author;
-import ru.mcclinics.orderservice.domain.Track;
 import ru.mcclinics.orderservice.service.AuthorService;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j(topic = "order-service")
--- src/main/java/ru/mcclinics/orderservice/rest/TrackController.java
+++ src/main/java/ru/mcclinics/orderservice/rest/TrackController.java
@@ -8,6 +8,7 @@ import ru.mcclinics.orderservice.domain.Track;
 import ru.mcclinics.orderservice.service.TrackService;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -31,6 +32,27 @@ public class TrackController {
     public Track create(@RequestBody Track track){
         log.info("/create [track {}]", track);
         return service.create(track);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message){
+        Map<String, String> messageFromDb = getMessage("id");
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+        return messageFromDb;
+    }
+
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@RequestBody Track track){
+        Map<String, String> message = getMessage(id);
+        messages.remove(message);
+    }
+
+    private Map<String, String> getTrack(String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 
 
