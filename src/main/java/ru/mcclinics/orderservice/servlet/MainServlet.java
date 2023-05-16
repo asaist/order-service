3de4770f4260dc2726ec5d45a988ea01ead5e248
Vladimir <kosakvova@gmail.com>
2023-05-16 14:12:39 +0300

    tomcat keycloak_fix

---
+++ src/main/java/ru/mcclinics/orderservice/servlet/MainServlet.java
@@ -0,0 +1,24 @@
+package ru.mcclinics.orderservice.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
