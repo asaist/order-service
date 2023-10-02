368fad51207c69acc56ca690f17f18b50b48387f
Vladimir <kosakvova@gmail.com>
2023-10-02 15:07:41 +0400

    hide lecture contact-tab pane

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mvn clean install -U -P local
+
+echo 'Copy files...'
+
+scp -i ~/.ssh/id_rsa \
+    C:/Users/Vladimir/IdeaProjects/order-service/target/order-service-1.0.jar \
+    kvn@172.26.0.62:/home/kvn/
+
+echo 'Restart server...'
+
+ssh -i ~/.ssh/id_rsa kvn@172.26.0.62 << EOF
+pgrep java | xargs kill -9
+nohup java -jar order-service-1.0.jar > log.txt &
+EOF
+
+echo 'Bye'
